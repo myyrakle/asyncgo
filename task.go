85cfdb95c506f29ca8240589a79fb4d881c5d24a
myyrakle <sssang97@naver.com>
2024-02-04 01:22:05 +0900

    add: RunPanicableTask

--- task.go
+++ task.go
@@ -1,5 +1,7 @@
 package async
 
+import "fmt"
+
 type runTaskFuture[T any] struct {
 	ch chan T
 }
@@ -19,6 +21,28 @@ func RunTask[T any](f func(args ...any) T, args ...any) Future[T] {
 	future := runTaskFuture[T]{ch: ch}
 
 	go func() {
+		defer close(ch)
+		ch <- f(args...)
+	}()
+
+	return &future
+}
+
+// RunPanicableTask runs a function asynchronously and returns a Future that will contain the result.
+func RunPanicableTask[T any](f func(args ...any) Result[T], args ...any) Future[Result[T]] {
+	ch := make(chan Result[T])
+
+	future := runTaskFuture[Result[T]]{ch: ch}
+
+	go func() {
+		// panic recover
+		defer func() {
+			if r := recover(); r != nil {
+				err := fmt.Errorf("panic recover: %v", r)
+				ch <- Err[T](err)
+			}
+		}()
+
 		defer close(ch)
 		ch <- f(args...)
 	}()
