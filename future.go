b52fd15f3d5ffd43d63a73f7d365011455a25de6
myyrakle <sssang97@naver.com>
2024-02-09 08:25:11 +0000

    [#1] refactor JoinAll

--- future.go
+++ future.go
@@ -11,32 +11,7 @@ type Future[T any] interface {
 // RunTask runs a function asynchronously and returns a Future that will contain the result.
 func JoinAll[T any](futures ...Future[T]) Future[[]T] {
 	return RunTask(func(args ...any) []T {
-		length := len(futures)
-
-		// 채널을 전부 받아옵니다.
-		channels := make([]chan T, length)
-		for i, future := range futures {
-			channels[i] = future.Channel()
-		}
-		results := make([]T, length)
-		doneList := make([]bool, length)
-		doneCount := 0
-
-		// 모든 채널이 끝날 때까지 기다립니다.
-		for doneCount < length {
-			for i, ch := range channels {
-				if !doneList[i] {
-					select {
-					case result := <-ch:
-						results[i] = result
-						doneList[i] = true
-						doneCount++
-					default:
-						continue
-					}
-				}
-			}
-		}
+		results := AwaitAll(futures...)
 
 		return results
 	})
