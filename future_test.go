0c3327da8a12f6ac2824124e98dfa9573f1f8de4
myyrakle <sssang97@naver.com>
2024-02-09 23:19:19 +0000

    [#3] add testcase: TestJoinAll

--- future_test.go
+++ future_test.go
@@ -46,3 +46,45 @@ func TestAwaitAll(t *testing.T) {
 		})
 	}
 }
+
+func TestJoinAll(t *testing.T) {
+	tests := []struct {
+		name    string
+		futures []Future[int]
+		want    []int
+	}{
+		{
+			name: "덧셈 task 목록 실행",
+			futures: []Future[int]{
+				RunTask(func(args ...any) int {
+					lhs := args[0].(int)
+					rhs := args[1].(int)
+
+					return lhs + rhs
+				}, 10, 20),
+				RunTask(func(args ...any) int {
+					lhs := args[0].(int)
+					rhs := args[1].(int)
+
+					return lhs + rhs
+				}, 15, 2),
+				RunTask(func(args ...any) int {
+					lhs := args[0].(int)
+					rhs := args[1].(int)
+
+					return lhs + rhs
+				}, 1, 20),
+			},
+			want: []int{
+				30, 17, 21,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := JoinAll(tt.futures...).Await(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("JoinAll() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
